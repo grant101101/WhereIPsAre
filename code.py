c1e7d4bbe40c45cb55aca893a425d9c5d94dc5a7
OmarGQ <ogodinezquinones@monmouthcollege.edu>
2021-04-20 11:58:24 -0500

    Added files

---
+++ code.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 20 11:50:50 2021
+
+@author: kiddra
+"""
+class project:
+    def step1 (self):
+        print("step1")
+        
+    def step2(self):
+        print("step2")
+        
+    def step3(self):
+        print("step3")
+        
+    def step4(self):
+        print("step4")
+        
+    def step5(self):
+        print("step5")
+        
+p = project()
+
