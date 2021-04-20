c0dbb9dd3224402ddf98ba726325bebfb8746754
OmarGQ <ogodinezquinones@monmouthcollege.edu>
2021-04-20 17:30:08 -0500

    first step ready

--- code.py
+++ code.py
@@ -4,11 +4,40 @@ Created on Tue Apr 20 11:50:50 2021
 
 @author: kiddra
 """
+import csv
+import re
+#fail2ban.actions
+
+
 class project:
-    def step1 (self):
-        print("step1")
+    def step0 (self):
+        with open('interest.csv', 'w', newline='') as file:
+            writer1 = csv.writer(file)
+            with open('ignore.csv', 'w', newline='') as file:
+                writer2 = csv.writer(file)
+                for x in range(14):
+                    with open('fail2banlogs/fail2ban.log.'+ str(x), newline='') as csvfile:
+                        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+                        for row in spamreader:
+                            m = re.search( 'NOTICE', str(row))
+                            if m:
+                                writer1.writerow(row)
+                            else:
+                                writer2.writerow(row)
         
-    def step2(self):
+    def step1(self, interest, ignore):
+        with open('IPs.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            with open(interest, newline='') as csvfile:
+                spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+                for row in spamreader:
+                    date = re.search(r'\d{4}[-/]\d{2}[-/]\d{2}',str(row)).group()
+                    time = re.search(r'\d{2}[:/]\d{2}[:/]\d{2}[,/]\d{3}', str(row)).group()
+                    ip = re.search(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", str(row)).group()
+                    inf = [date, time, ip]
+                    writer.writerow(inf)
+        
+    def step2(self, interest, ignore):
         print("step2")
         
     def step3(self):
@@ -21,4 +50,4 @@ class project:
         print("step5")
         
 p = project()
-
+p.step1("interest.csv", "ignore.csv")
