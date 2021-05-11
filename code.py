08d1ec1ff37abac1fb4b206660005c8588e55a1e
Grant Miller <gemiller@monmouthcollege.edu>
2021-05-11 09:41:35 -0500

    Made changes

--- code.py
+++ code.py
@@ -10,9 +10,14 @@ import re
 import pandas as pd
 import plotly as pl
 from ipstack import GeoLookup
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.offline import plot
 #fail2ban.actions
 
 #data = pd.DataFrame()
+
+pd.set_option('display.max_columns',None)
 
 class project:
     def step0 (self):
@@ -45,6 +50,7 @@ class project:
     def step2(self):
         print("step2")
         df = pd.read_csv("IPs.csv", names=['date','ip'], header=None)
+        df['date']=pd.to_datetime(df['date'])
         print(df)
         # how many unique IPs?
         print(df.nunique())
@@ -57,7 +63,7 @@ class project:
         unique = df['ip'].unique()
         # ryan m's API key
         # careful: limited to 10,000 requests
-        gl = GeoLookup("ba1b4b735b7c286f04a392635dc6719e")
+        gl = GeoLookup("f26f5007b5f7a0a3501682919778805c")
 
         cols = ['ip','continent_code','continent_name','country_name','country_code','latitude','longitude']
         tempData = []
@@ -80,12 +86,58 @@ class project:
 
     def step4(self):
         print("step4")
+        df = pd.read_csv('complete.csv', usecols= ['date','ip','continent_name','continent_code','country_code','country_name','latitude','longitude'])
+        #df['date']=pd.to_datetime(df['date'])
+        #df.index=pd.to_datetime(df['date'])
+        #print(df)
+        country_value_counts=df['country_code'].value_counts()
+        valuesdf=pd.DataFrame(country_value_counts)
+        valuesdf=valuesdf.reset_index()
+        valuesdf.columns=['country_code','code_count']
+        print(valuesdf)
+        df['size']=pd.Series([131368309 for x in range(len(df.index))],index=df.index)
+        
+       #scatterplot 
+        fig1=go.Figure(data=go.Scattergeo(lon=df['longitude'],lat=df['latitude'],
+                                         text=df['ip'],mode='markers', marker_color='purple',
+                                         ))
+        fig1.update_layout(title='IP Address Locations Across the World')
+        #plot(fig1)
+        
+        #histogram
+        fig2=px.histogram(valuesdf,x='country_code',y='code_count',
+                          labels={'country_code':'Country Code',
+                                  'code_count':'Number of IPs'})
+        fig2.update_layout(title='Number of IPs From Each Country')
+        #plot(fig2)
+        
+        countrylat=[35,38,16,64,51,37,-33,22.25,60,43,46,54]
+        countrylon=[105,-97,106,26,9,127,-56,114.1667,100,25,2,-2]
+        valuesdf['country_lat']=countrylat
+        valuesdf['country_lon']=countrylon
+        valuesdf['code_count']=valuesdf['code_count']
+        #print(valuesdf)
+        fig3=px.scatter_geo(valuesdf,lat='country_lat',lon='country_lon',size='code_count',projection='natural earth',title='Bubble Map of IPs')
+        #plot(fig3)
+        
+        fig4=go.Figure(data=[go.Table(
+            header=dict(values=['Country Code','Country Count'],
+                        fill_color='paleturquoise',align='left'),
+            cells=dict(values=[valuesdf['country_code'],valuesdf['code_count']],
+                               fill_color='lavender',align='left'))
+                               ])
+        
+        plot(fig4)
+
+        
+        print("step4")
         
     def step5(self):
         print("step5")
         
 p = project()
-p.step0()
-p.step1("interest.csv", "ignore.csv")
-p.step2()
-p.step3()
+#p.step0()
+#p.step1("interest.csv", "ignore.csv")
+#p.step2()
+#p.step3()
+p.step4()
